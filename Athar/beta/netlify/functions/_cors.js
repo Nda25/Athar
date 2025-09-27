0d77aab0a35f74019e0a8f4da5ca83606880955f
Nda25 <nooshe29@gmail.com>
2025-09-27 14:39:56 +0300

    Create _cors.js

---
+++ Athar/beta/netlify/functions/_cors.js
@@ -0,0 +1,20 @@
+// /.netlify/functions/_cors.js
+// Helper موحّد لإعدادات CORS
+
+const ORIGIN = process.env.ALLOWED_ORIGIN || "*";
+
+// كائن الـ Headers الجاهز للإرجاع مع كل Response
+exports.CORS = {
+  "Access-Control-Allow-Origin": ORIGIN,
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+  "Access-Control-Allow-Methods": "GET,POST,OPTIONS,DELETE,PUT",
+  "Vary": "Origin"
+};
+
+// هيلبر للتعامل مع Preflight requests
+exports.preflight = (event) => {
+  if (event.httpMethod === "OPTIONS") {
+    return { statusCode: 204, headers: exports.CORS, body: "" };
+  }
+  return null;
+};
